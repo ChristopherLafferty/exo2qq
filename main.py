058bc2069c10eab7d74ddc4394cf25ca52bf69c6
Christopher Lafferty <christopher.e.lafferty@gmail.com>
2025-05-13 12:18:41 -0400

    Start DateTime Normalize Feature added - Added new section with summary info section - Added Date and Time Widgets and Form Submit to allow for DateTime normalizing.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
-import altair as alt
+import datetime
+import numpy as np
 
 QQ_HEADERS = ['QQSN,Ch0SN,Ch1SN,Ch2SN,Ch0Cal,Ch1Cal,Ch2Cal,FWVers,VolInj(ml),CalVol(l),[NaCl](g/l),StnName,StnID,PrjName,PrjID',
               'QM0.00,TM0.000,TM0.000,TM0.000,0.0,0.0,0.0,QQF0.0.00,0.000,0.000,0.000,00000000 - NA,0,NONE,0',
@@ -22,24 +23,22 @@ def exo_to_qq(df):
                         'Temp °C':'Temp(oC)',
                         'SpCond µS/cm': 'EC.T(uS/cm)'},
                 inplace=True)
-        df['DateTime'] = df['Date (MM/DD/YYYY)'] + ' ' + df['Time (HH:mm:ss)'] 
+        df['DateTime'] = pd.to_datetime(df['Date (MM/DD/YYYY)'] + ' ' + df['Time (HH:mm:ss)'])
 
         # Create new df using just the fields needed for QQ
         df_out = df[['DateTime', 'EC(uS/cm)', 'Temp(oC)','EC.T(uS/cm)']]
 
         # Add Placeholder Columns for QQ
-        for col in ['Mass(kg)', 'CF.T(mg/L)/(uS/cm)', 'BGEC.T(uS/cm)',
-                    'Q(cms/cfs)', 'Grade', 'S_BGEC.T(uS/cm)', '2sUnc_Q(%)',
-                    'preBG_index', 'dt(s)', 'Area(s*uS/cm)', '3rd_U/S',
-                    'QUnit', 'QComp', 'VBatt(V)' ]:
-            df_out[col] = ''
-
+        cols = ['Mass(kg)', 'CF.T(mg/L)/(uS/cm)', 'BGEC.T(uS/cm)',
+                'Q(cms/cfs)', 'Grade', 'S_BGEC.T(uS/cm)', '2sUnc_Q(%)',
+                'preBG_index', 'dt(s)', 'Area(s*uS/cm)', '3rd_U/S',
+                'QUnit', 'QComp', 'VBatt(V)' ]
+        df_out.loc[:, cols] = ''
         return df_out
     
     except:
         st.write("Error: A problem was encountered in conversion. EXO was not in expected format.")
         return None
-
 
 def read_exo(file):
     try:
@@ -52,29 +51,42 @@ def read_exo(file):
         st.write(":red[Error]: There was a problem reading EXO CSV file")
         return None
 
-def display_qq_preview(df):
-    row_count, col_count = df.shape
-    st.write(f'Preview of {row_count:,d} rows of data: ')
-    df[0:min(10, row_count)] # Preview up to first 10
-    st.line_chart(df,
-                    x= 'DateTime',
-                    y=['EC(uS/cm)', 'Temp(oC)', 'EC.T(uS/cm)' ],
-                    color=['#FF0000', '#00FF00', '#0000FF']
-                    )
-
 def main():
     st.title("EXO CSV to QQ CSV Converter")
+
+    # with st.container():
     uploaded_file = st.file_uploader('Select your EXO CSV File:', type="csv")
-    st.divider()
-    
-    if uploaded_file:        
+
+    # st.divider()
+    if uploaded_file:
         exo_df = read_exo(uploaded_file)
 
-        if exo_df is not None:
-            
+        if exo_df is not None:            
             qq_df = exo_to_qq(exo_df)
 
-            if qq_df is not None:                
+            if qq_df is not None:
+                with st.container(border=True):
+                    col1, col2 = st.columns(2)
+                    start_date_base = qq_df['DateTime'][0]
+                    with col1:
+                        f"First DateTime: :green[{start_date_base}] "
+                        f"Records Read: :green[{qq_df.shape[0]:,d}]"
+                    
+                    with col2:
+                        with st.form(key='info_form'):
+                            t = st.time_input("Start Time", value=start_date_base.time(), key='start_time')
+                            d = st.date_input("Start Date", value=start_date_base.date(), key='start_date')
+                            submit = st.form_submit_button('Normalize DateTime', type='secondary')
+
+                if submit:
+                    form_time = pd.to_datetime(
+                        f"{st.session_state['start_date'].isoformat()} {st.session_state['start_time'].isoformat()}")
+                    time_adjust = form_time - start_date_base
+                    
+                    # qq_df['DateTime'] = qq_df['DateTime'] + form_time
+                    qq_df.loc[:, 'DateTime'] += time_adjust           
+
+
                 csv_out = '\r\n'.join(QQ_HEADERS) + qq_df.to_csv(index=False, lineterminator = '\r\n', encoding='utf-8')
                 st.download_button(
                     label="Download QQ CSV",
@@ -84,9 +96,22 @@ def main():
                     icon=':material/download:',
                     type='primary'
                 )
-                st.divider()
+
                 st.subheader("Previews:")
-                display_qq_preview(qq_df)            
+
+                tab_data_preview, tab_chart_preview = st.tabs(["🗃 Data", "📈 Chart"])
+                # Data Tab                
+                with tab_data_preview:
+                    preview_lines_count = min(10, qq_df.shape[0])
+                    st.write(f'Previewing {preview_lines_count} of {qq_df.shape[0]:,d} rows')
+                    st.dataframe(qq_df[0:preview_lines_count]) # Up to 10 rows in preview
+                # Chart Tab
+                with tab_chart_preview:
+                    st.line_chart(qq_df,
+                                    x= 'DateTime',
+                                    y=['EC(uS/cm)', 'Temp(oC)', 'EC.T(uS/cm)' ],
+                                    color=['#FF0000', '#00FF00', '#0000FF']
+                                    )
 
     st.divider()
     st.markdown(
